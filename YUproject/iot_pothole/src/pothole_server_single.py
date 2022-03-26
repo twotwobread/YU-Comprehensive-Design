3102202fc7c5ecac3f954df469da06292b9cbba2
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-03-26 23:37:09 +0900

    Update pothole_server_single.py

--- YUproject/iot_pothole/src/pothole_server_single.py
+++ YUproject/iot_pothole/src/pothole_server_single.py
@@ -26,6 +26,7 @@ import base64
 from datetime import datetime
 import pymysql
 
+# IP, API_KEY, DB 연결, 소켓으로 보낼 데이터를 위한 버퍼, 임계구역을 위한 초기 세팅
 IP=""
 API_KEY = ""
 CONN_DB = pymysql.connect(host='127.0.0.1', user='root', password='', db='pothole', charset='utf8')
@@ -35,45 +36,52 @@ LOCK = threading.Lock()
 
 ####################[ web server using flask ]###########################
 
+# flask를 이용해서 web server 기능을 구현하기 위함.
 webRemoteControlServer = Flask(__name__)
 
-@webRemoteControlServer.route("/") # root web site (pothole_server.html)
+@webRemoteControlServer.route("/") # root web site (pothole_server.html) # pothole_server.html : 검색 기능이 있는 초기 화면을 띄움.
 def index():
     return render_template('pothole_server.html')
 
 @webRemoteControlServer.route("/result", methods=['GET', 'POST']) # root web site (pothole_dataPrint.html)
 def result_fuction():
-    if request.method == 'POST':
+    if request.method == 'POST': # 검색 기능을 사용할 때 무조건 post를 이용
         address = request.form
     print(len(address))
     # pothole_server.html에서 request data가 비어있으면 
     # 검색 시 문제가 생길 수 있다 생각하여 넣은 구문
-    total_addr="";
+    total_addr=""; # 최종적으로 찾고 싶은 주소를 담기 위한 변수
     cnt = 0
     list_addr = []
+    # 결과적으로는 post로 날라온 address 정보를 이용해서 쿼리문을 짜고 DB에서 값을 들고 와야함. ( address는 array 형식으로 날라옴 )
+    # 만약 address 중간이 ""가 아니라도 '%' 얘를 붙여도 되는지 생각해보자
+    # 경상북도%경산시%삼풍동 이렇게 해도 될거 같은데 일단 확인을 해봐야할 부분인 것 같다.
+    # 만약 address 중간이 ""면 '%' 이놈이 무조건 필요할 것이다.
+    # 그걸 위한 if문인 것 같다.
     if address['1']!="" and address['2']=="" and address['3']!="":
         total_addr = address['1']+"%"+address['3']
-    else:
+    else: # 중간만 ""인 경우 말고 전부.
         for i in range(len(address)):
             str_num = str(i+1)
-            if address[str_num] == "":
+            if address[str_num] == "": # 이부분을 만나면 마지막이라는 의미.
                 pass
-            else:
-                if cnt==0:
-                    total_addr=address[str_num]
-                else:
-
-                    total_addr+=" "+address[str_num]
-                cnt+=1
-    real_addr = "%"+total_addr+"%"
+            else: # 마지막이 아니면
+                if cnt==0: # 맨 처음인 경우를 생각
+                    total_addr=address[str_num] # total address에 주소를 넣음
+                else:# 맨 처음이 아니면
+                    total_addr+=" "+address[str_num] # 공백을 넣기 위함.
+                cnt+=1 # cnt 하나씩 증가시키면서 
+    real_addr = "%"+total_addr+"%" # 마지막으로 앞뒤에 '%'를 붙여줌으로 다 찾을 수 있게 하는데
+    # 이걸 봤을 때, %경상북도%경산시%삼풍동% 이것도 가능할꺼 같다.
     print(real_addr)
-    sql = "SELECT * FROM img_path WHERE addr LIKE %s"
+    sql = "SELECT * FROM img_path WHERE addr LIKE %s" # DB에서 데이터를 찾기 위한 쿼리문
     CUR_DB.execute(sql, real_addr) # get DB data that addr(attribute) equal to real_addr 
-    CONN_DB.commit()
-    result = CUR_DB.fetchall()
+    # 모든 데이터를 찾기위해 '%'를 이용 해서 쿼리문 완성
+    CONN_DB.commit() # DB로 접근
+    result = CUR_DB.fetchall() # 해당하는 array를 들고옴.
     print(result)
    
-    return render_template('pothole_dataPrint.html', data=result)
+    return render_template('pothole_dataPrint.html', data=result) # data로 result를 보내면서 pothole_dataPrint.html 띄움.
 
 ##################[ socket & data manage ]########################
 
@@ -104,6 +112,9 @@ class ServerSocket: # class for communication with clients
         cnt=0.0
         try:
             while True:
+                # 이 부분은 사진을 받기 위한 부분 사진을 받기 위해서 사진의 크기가 있으니까 잘려서 날라올테니까 먼저 잘려날라오는 길이를 받고
+                # decode해서 길이를 파악한 후 길이만큼 데이터를 전부 다 받아서 버퍼에 넣어놓고 decode해서 이용
+                # 그리고 gps 정보도 소켓을 이용해서 받아주는 부분.
                 length = self.recvall(self.conn, 64) # first, receive length of one image
                 length = length.decode('utf-8') # decode length data because encoded length data fly away 
                 stringData = self.recvall(self.conn, int(length)) 
@@ -116,19 +127,22 @@ class ServerSocket: # class for communication with clients
                 print("longitude : ", self.gps_longitude)
                 decimg = cv2.imdecode(data, 1)
                 print("get img file !!!!")
+                # 아래 라인이 카카오 지도 api에 위도, 경도를 넣어서 request를 하기 위한 url을 만드는 라인
                 base_url='https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x='+str(self.gps_longitude)+"&y="+str(self.gps_latitude)+'&input_coord=WGS84'
+                # 이미지를 저장하기 위해서 이미지의 이름을 위도, 경도를 이용해서 만듬.
+                # 소숫점 5번째까지 trunk하여 대략 1m 이내의 포트홀은 같은 포트홀로 인식.
                 path=str(self.gps_latitude)+'_'+str(self.gps_longitude)+'.jpg' 
-                sql_path="SELECT * FROM img_path WHERE img_path=%s;"
+                sql_path="SELECT * FROM img_path WHERE img_path=%s;" # 쿼리문을 작성하고 이미지가 있는 지 먼저 확인 
                 CUR_DB.execute(sql_path, path)
                 result = CUR_DB.fetchall()
-                if len(result) == 0: # data 중복 저장을 막기 위함.
-                    cv2.imwrite("static/"+path, decimg)
-                    val = (path, str(self.gps_latitude), str(self.gps_longitude), base_url)
+                if len(result) == 0: # data 중복 저장을 막기 위함. 이미지가 없는 경우, 이 부분 개선하면 좋을 듯.
+                    cv2.imwrite("static/"+path, decimg) # static 디렉토리에 이미지를 저장
+                    val = (path, str(self.gps_latitude), str(self.gps_longitude), base_url) # DB에 넣을 정보를 튜플로 구성
                     
                     LOCK.acquire()
-                    SOCKET_DATA.append(val)
+                    SOCKET_DATA.append(val) # 소켓 데이터에 넣음, 이 부분도 개선 가능할 것 같음.
                     LOCK.release()
-                else:
+                else: # 이미지가 존재하는 경우
                     print("같은 위치의 포트홀입니다!!!!!")
                 #cv2.imshow("image", decimg)
                 #cv2.waitKey(1)
@@ -170,20 +184,20 @@ def save_data():
         for i in SOCKET_DATA: # SOCKET_DATA에 값이 있으면 DB에 저장
             print(SOCKET_DATA)
             headers={'Authorization':'KakaoAK '+API_KEY}
-            api_req=requests.get(i[3], headers=headers)
+            api_req=requests.get(i[3], headers=headers) # kakao api에 실제로 request하는 부분
             jsonObjects=json.loads(api_req.text)
-            if jsonObjects.get('msg') == None:
-                address=jsonObjects.get('documents')[0]['address_name']
+            if jsonObjects.get('msg') == None: # 제대로 값을 들고 왔는지를 확인하기 위한 부분.
+                address=jsonObjects.get('documents')[0]['address_name'] # 위도 경도에 맞는 주소를 가져오는 부분.
                 print("database start")
                 LOCK.acquire()
-                sql = "INSERT INTO img_path ( img_path, latitude, longitude, addr ) VALUES (%s, %s, %s, %s);"
-                val = (i[0], i[1], i[2], address)
+                sql = "INSERT INTO img_path ( img_path, latitude, longitude, addr ) VALUES (%s, %s, %s, %s);" # 주소까지 포함해서 쿼리문 작성
+                val = (i[0], i[1], i[2], address) # DB에 저장.
                 CUR_DB.execute(sql, val)
                 CONN_DB.commit()
                 LOCK.release()
             else:
                 print("잘못된 위도 경도 입니다 !!")
-            SOCKET_DATA.pop(0)
+            SOCKET_DATA.pop(0) # 맨 처음들어온 주소부터 처리하기 위함.
             print("database end")
         time.sleep(0.1) 
 
@@ -197,7 +211,7 @@ def main(): # web server run
 #########################[main]##############################
 
 if __name__ == "__main__":
-    save_data_thread = threading.Thread(target=save_data)
+    save_data_thread = threading.Thread(target=save_data) # 저장하는 부분이랑 데이터를 받는 부분을 따로 만들어서 스레드로 동작.
     save_data_thread.start()
     gps=ServerSocket(IP, 20)
     main()
